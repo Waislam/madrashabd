753c92d8d7e396fbbdc365ac2a4f322e0161ed47
MHMasuk <masuk@w3engineers.com>
2022-08-11 00:07:25 +0600

    Fix update serializer in madrasha.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -91,21 +91,27 @@ class MadrashaSerializer(serializers.ModelSerializer):
         return madrasha
 
     def update(self, instance, validated_data):
-        madrasha_name = validated_data['name']
-        madrasha_id = validated_data['madrasha_id']
-        slug = validated_data['slug']
+        instance.name = validated_data.get('name')
+        instance.address = validated_data.get('madrasha_address')
 
-        # create address
-        address = Address.objects.update(**validated_data['madrasha_address'])
+        # Get madrasha address
+        madrasha_address = Address.objects.get(pk=instance.madrasha_address.pk)
 
-        # create madrasha
-        madrasha = Madrasha.objects.update(
-            name=madrasha_name,
-            madrasha_address=address,
-            madrasha_id=madrasha_id,
-        )
+        # Save madrasha address
+        madrasha_address.division = validated_data.get('madrasha_address').get('division')
+        madrasha_address.district = validated_data.get('madrasha_address').get('district')
+        madrasha_address.post_office = validated_data.get('madrasha_address').get('post_office')
+        madrasha_address.post_code = validated_data.get('madrasha_address').get('post_code')
+        madrasha_address.thana = validated_data.get('madrasha_address').get('thana')
+        madrasha_address.address_info = validated_data.get('madrasha_address').get('address_info')
+        madrasha_address.save()
 
-        return madrasha
+        instance.created_by = validated_data.get('created_by')
+        instance.updated_by = validated_data.get('updated_by')
+        instance.active_status = validated_data.get('active_status')
+        instance.save()
+
+        return instance
 
 
 # ================== 3. User ============
