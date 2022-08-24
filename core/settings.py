e7912282f95d85fb4f27923f4bc0519b9594dd77
Waislam <waislam67@gmail.com>
2022-08-24 21:16:06 +0600

    corsheader middleware added

--- core/settings.py
+++ core/settings.py
@@ -49,6 +49,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # for corsheaders
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'core.urls'
