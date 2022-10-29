be6347624b8a4851d23f9635468da78ec6e7f2bb
jayed969 <jayed.swe@gmail.com>
2022-10-29 14:36:24 +0600

    change urls

--- boarding/views.py
+++ boarding/views.py
@@ -1,37 +1,36 @@
 from rest_framework import status, generics
-from rest_framework.generics import mixins
+from rest_framework.generics import mixins, GenericAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from .models import BazarList
 from .serializers import BazarListSerializer
 from .filters import BazarListFilter
-from accounts.models import Madrasha
 from .pagination import CustomPagination
 
 
 class BazarListView(
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
-    generics.GenericAPIView
+    GenericAPIView
 ):
-    """BazarList Create and list view"""
-
-    # queryset = BazarList.objects.all()
+    queryset = BazarList.objects.all()
     filter_backends = [DjangoFilterBackend, SearchFilter]
-    filterset_class = BazarListFilter
-    search_fields = ["date"]
-    pagination_class = CustomPagination
     serializer_class = BazarListSerializer
+    filterset_class = BazarListFilter
+    search_fields = ['date']
+    pagination_class = CustomPagination
 
     def get_queryset(self):
-        slug = self.kwargs['slug']
-        return BazarList.objects.filter(madrasha__slug=slug)
+        """getting any argument/parameter from api/url"""
+        madrasha_slug = self.kwargs['madrasha_slug']
+        return super(BazarListView, self).get_queryset().filter(madrasha__slug=madrasha_slug)
+        # return super(BazarListView, self).get_queryset()
 
-    def get(self, request,  *args, **kwargs):
-        """method to show the list of Students"""
-
+    def get(self, request, *args, **kwargs):
+        """method to show the list of Teacher """
         return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        """Method to create student obj"""
+        """Method to create Teacher obj """
+        # print('kwargs', **kwargs)
         return self.create(request, *args, **kwargs)
