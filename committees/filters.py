a46e17f6c25039896b62f61066641d0b5ac41ef4
jayed969 <jayed.swe@gmail.com>
2022-11-01 19:28:39 +0600

    added PermanentMembers & others Members api done

--- committees/filters.py
+++ committees/filters.py
@@ -1,4 +1,4 @@
-from .models import Committee
+from .models import Committee, PermanentMembers, OtherMembers
 import django_filters
 
 
@@ -8,3 +8,13 @@ class CommitteeFilter(django_filters.FilterSet):
         fields = ['phone_number', 'member_name']
 
 
+class PermanentMembersFilter(django_filters.FilterSet):
+    class Meta:
+        model = PermanentMembers
+        fields = ['phone_number', 'member_name']
+
+
+class OtherMembersFilter(django_filters.FilterSet):
+    class Meta:
+        model = OtherMembers
+        fields = ['phone_number', 'member_name']
--- committees/serializers.py
+++ committees/serializers.py
@@ -1,5 +1,9 @@
 from rest_framework import serializers
-from .models import Committee
+from .models import (
+    Committee,
+    PermanentMembers,
+    OtherMembers
+)
 
 
 class CommitteeListSerializers(serializers.ModelSerializer):
@@ -24,5 +28,57 @@ class CommitteeSerializers(serializers.ModelSerializer):
             'madrasha',
             'member_name',
             'member_designation',
+            'phone_number'
+        ]
+
+
+class PermanentMembersListSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = PermanentMembers
+        fields = [
+            'id',
+            'madrasha',
+            'member_name',
+            'address',
+            'phone_number'
+        ]
+
+        depth = 2
+
+
+class PermanentMembersSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = PermanentMembers
+        fields = [
+            'id',
+            'madrasha',
+            'member_name',
+            'address',
+            'phone_number'
+        ]
+
+
+class OthersMemberListSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = OtherMembers
+        fields = [
+            'id',
+            'madrasha',
+            'member_name',
+            'address',
+            'phone_number'
+        ]
+
+        depth = 2
+
+
+class OthersMemberSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = OtherMembers
+        fields = [
+            'id',
+            'madrasha',
+            'address',
+            'address',
             'phone_number'
         ]
