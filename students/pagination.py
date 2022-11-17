9c2d3ca9343731eee06acbe5677c1baa0c38d073
ahanafislam <ahanafislam@gmail.com>
2022-11-17 20:20:02 +0600

    Cdd

--- students/pagination.py
+++ students/pagination.py
@@ -2,10 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
-<<<<<<< HEAD
     page_size = 10
-=======
-    page_size = 3
->>>>>>> d6bb27bf7b982d4893faaee6150f762e3ec2bd27
     page_size_query_param = 'records'
     max_page_size = 10
